dcfef7888a1a7b2ce07bf5d96e8b745abcab2e9d
Alessandro <alessandroadm@live.com>
2020-12-08 22:09:49 -0300

    feat: add carousel

--- src/resources/layouts/default/index.tsx
+++ src/resources/layouts/default/index.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 import Nav from '../../components/nav';
+import Carousel from '../../components/banner';
 
 export default () => (
   <>
@@ -7,7 +8,18 @@ export default () => (
       <header className='header'>
         <Nav></Nav>
       </header>
-      <section className='section'>Main section</section>
+
+      <section className='section'>
+        <article
+          style={{
+            overflow: 'hidden',
+            maxWidth: '98.7vw',
+          }}
+        >
+          <Carousel />
+        </article>
+        Main section
+      </section>
       <footer className='footer'>Footer</footer>
     </main>
   </>
